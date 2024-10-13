59895a86e61647b31eb56d9a394bec1b2efceda6
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-10-12 20:04:23 -0400

    init commit

---
+++ musicShapes1.0/src/assets/colorScroll.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+let lastColor = ''
+
+function getRandomColor(hexColor, colorArray, names) {
+    // Check if the input color has changed
+    if (hexColor !== lastColor) {
+      
+        
+        // Generate a random index to select a color from the array
+        const randomIndex = Math.floor(Math.random() * colorArray.length);
+        const nameIndex = Math.floor(Math.random() * names.length)
+        let output = {color: colorArray[randomIndex], name: names[nameIndex]};
+     
+        return output
+       
+    
+
+
+}
+}
+
+export default getRandomColor;
