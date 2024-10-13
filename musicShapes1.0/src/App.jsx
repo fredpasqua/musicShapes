4190bf1510a9880681002ab8dcc6809e724a1315
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-10-12 20:31:18 -0400

    updates

--- musicShapes1.0/src/App.jsx
+++ musicShapes1.0/src/App.jsx
@@ -1,20 +1,13 @@
 import { useState, useEffect } from 'react'
-import Gameboard from "./App2.jsx";
+import Gameboard from "./GameBoard.jsx";
 import getRandomColor from "./assets/colorScroll.jsx";
 import "./App.css";
 import Countdown from "react-countdown";
 
 function App() {
-  const [color1, setColor1] = useState({ color: "#FF5733", name: "1" });
-  const [color2, setColor2] = useState({ color: "#FF5733", name: "2" });
-  const [color3, setColor3] = useState({ color: "#FF5733", name: "3" });
-  const [color4, setColor4] = useState({ color: "#FF5733", name: "4" });
-  const [color5, setColor5] = useState({ color: "#FF5733", name: "5" });
-  const [color6, setColor6] = useState({ color: "#FF5733", name: "6" });
   const [match, setMatch] = useState({ color: "#FF5733", name: "x" });
   const colors = ["#FF5733", "#33FF57", "#883982"];
   const names = ["x", "o"];
-  const [size, setSize] = useState(30);
   useEffect(() => {
     setMatch(getRandomColor(match.color, colors, names));
   }, []);
