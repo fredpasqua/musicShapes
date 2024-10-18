d3c7111bcc7fac3b4be5794df210ea75f065a89e
Fred Pasqua <95834438+fredpasqua@users.noreply.github.com>
2024-10-17 20:51:53 -0400

    add modal and function to close

--- musicShapes1.0/src/App.jsx
+++ musicShapes1.0/src/App.jsx
@@ -8,35 +8,48 @@ function App() {
   const [match, setMatch] = useState({ color: "#FF5733", name: "x" });
   const colors = ["#FF5733", "#33FF57", "#883982"];
   const names = ["x", "o"];
-  useEffect(() => {
-    setMatch(getRandomColor(match.color, colors, names));   
-  }, []);
+  const [showModal, setShowModal] = useState(true);
 
+  useEffect(() => {
+    setMatch(getRandomColor(match.color, colors, names));
+  }, [showModal]);
+  const closeModal = () => {
+    setShowModal(false);
+  };
   return (
-    <div className="screen">
-      <div className="gameHeader">
-        {" "}
-        <h4>Set the letter and color of each card to</h4>
-        <br></br>
-        <button
-          className={"matchButton gameButton"}
-          style={{
-            backgroundColor: match.color,
-            height: "100px",
-            width: "200px",
-          }}
-        >
-          {match.name}
-        </button>
-        <br></br>
-        <h4>All 6 cards must fit within the red line</h4>
-        <Countdown date={Date.now() + 45000} className="timer" />
+    <>
+      <div className="screen">
+        {showModal ? (
+          <div className="loser">
+            {" "}
+            <p>Sorry, try again!</p>
+            <button onClick={() => closeModal()}>button</button>
+          </div>
+        ) : null}
+        <div className="gameHeader">
+          {" "}
+          <h4>Set the letter and color of each card to</h4>
+          <br></br>
+          <button
+            className={"matchButton gameButton"}
+            style={{
+              backgroundColor: match.color,
+              height: "100px",
+              width: "200px",
+            }}
+          >
+            {match.name}
+          </button>
+          <br></br>
+          <h4>All 6 cards must fit within the red line</h4>
+          <Countdown date={Date.now() + 45000} className="timer" />
+        </div>
+        <div>
+          {" "}
+          <Gameboard className={"board"}></Gameboard>
+        </div>
       </div>
-      <div>
-        {" "}
-        <Gameboard className={"board"}></Gameboard>
-      </div>
-    </div>
+    </>
   );
 }
 
